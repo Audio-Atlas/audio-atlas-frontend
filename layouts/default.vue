520370fef1e3ac1aac24b55d559cbca5ba88da7f
smdbs <1289707349@qq.com>
2024-11-09 16:32:09 -0500

    feat: apply gradient bg to default layout
    Signed-off-by: smdbs <1289707349@qq.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex h-full flex-col items-center justify-center bg-background">
+  <div
+    class="flex h-full flex-col items-center justify-center bg-[url('/imgs/gradient.png')] bg-center bg-no-repeat"
+  >
     <nav class="space-x-2">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/about">About</NuxtLink>
