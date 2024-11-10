26a7b34dffba6e4c11a62f690d7a3e44e5e7c199
smdbs <1289707349@qq.com>
2024-11-10 17:22:31 -0500

    feat: create LinkButton and LinkButtonGroup components

---
+++ components/LinkButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <Button
+    variant="ghost"
+    size="icon"
+    as-child
+  >
+    <slot />
+  </Button>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
