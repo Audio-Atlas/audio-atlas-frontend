ed9224085969652a53ca2d385f854470f2e23df9
Matt <47070087+Camo651@users.noreply.github.com>
2024-11-10 13:18:32 -0500

    Added mp3 download button

---
+++ components/DownloadButton.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import DownloadIcon from 'vue-material-design-icons/Download.vue'
+
+const props = defineProps<{
+    url: string
+}>()
+
+const onDownload = () => {
+    window.open(props.url, '_blank')
+}
+</script>
+
+<template>
+    <div
+    class="
+        flex
+        items-center
+        gap-2
+    ">
+        <button
+        class="
+            flex
+            items-center
+            gap-2 border
+            border-white
+            px-6
+            py-3
+            text-white
+        "
+        @click="onDownload">
+            <DownloadIcon/>
+            MP3
+        </button>
+    </div>
+</template>
