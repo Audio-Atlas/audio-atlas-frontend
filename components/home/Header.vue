5297fbfddfa2d5e9c57173bcdfe12dd74a231423
smdbs <1289707349@qq.com>
2024-11-16 16:01:34 -0500

    feat: add HomeHeader component and change the default layout

---
+++ components/home/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="absolute top-0 flex w-full items-center justify-between px-8 pt-6"
+  >
+    <div class="invisible size-0" />
+    <LinkButtonGroup />
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,8 @@
 <template>
   <div
-    class="flex h-full flex-col items-center justify-center bg-[url('/imgs/gradient.png')] bg-center bg-no-repeat"
+    class="relative flex h-full flex-col items-center justify-center bg-[url('/imgs/gradient.png')] bg-center bg-no-repeat"
   >
-    <nav class="space-x-2">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
-    </nav>
+    <HomeHeader />
     <div>
       <slot />
     </div>
