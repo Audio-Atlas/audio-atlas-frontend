6d77b55718bc87e965185cb4c9cf3549a718a18e
smdbs <1289707349@qq.com>
2024-11-16 15:38:20 -0500

    feat: add event handler on enter to HomeSearchBar

--- components/HomeSearchBar.vue
+++ components/HomeSearchBar.vue
@@ -2,9 +2,11 @@
   <div class="relative h-[76px] w-[710px] items-center">
     <Input
       id="search"
+      v-model="query"
       type="text"
       placeholder="Find the perfect sound effect..."
       class="size-full rounded-none border-muted-foreground pl-6 pr-20 text-xl"
+      @keyup.enter="searchHandler"
     />
     <span
       class="absolute inset-y-0 end-0 flex items-center justify-center px-6"
@@ -19,4 +21,10 @@
 
 <script setup lang="ts">
 import { Input } from "@/components/ui/input";
+
+const query = ref("");
+const searchHandler = () => {
+  // TODO: Implement search functionality
+  console.log(query.value);
+};
 </script>
