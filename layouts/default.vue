20463d5c0be96b4820c5c680000a99215909fcf3
smdbs <1289707349@qq.com>
2024-11-24 18:57:07 -0500

    chore: refactor bg-img to body element

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative flex h-full flex-col items-center justify-center bg-[url('/imgs/gradient.png')] bg-center bg-no-repeat"
+    class="relative flex h-full flex-col items-center justify-center bg-center bg-no-repeat"
   >
     <HomeHeader />
     <div>
